9192a5c957006a5fbbbbdfaa80a9b6c724e896ea
Don Buck <dion-b@hotmail.com>
2018-11-09 16:47:31 +0000

    Functionin web app - generating new source word on refresh

--- WordGame/Final.py
+++ WordGame/Final.py
@@ -1,58 +1,35 @@
-#!/usr/bin/env python
+
 # coding: utf-8
 
-# In[12]:
+# In[13]:
 
 
-from random import randrange
+from random import choice, random
+from flask import Flask, render_template, request, session
 
 with open('./words.txt') as wordsf:
-    words0 = wordsf.readlines()
-# convert it to list() if printing, here just iterating (so save memory)
-words = (map(lambda each:each.strip('\n'), words0))
-
-# two helper functions from xword
-def suggest(hint_string):
-    """Take the hint_string and return the possible words."""
-    n_letters = len(hint_string)
-    known_letters = {}
-    for n, letter in enumerate(hint_string.lower()):
-        if letter != '-':
-            known_letters[n+1] = letter
-    return find_words(n_letters, known_letters)
-def find_words(length, letters):
-    """Find words of a certain length which contain
-       the specified positional letters."""
-    results = set()
+        words0 = wordsf.readlines()
+        # don't convert to list if just iterating (save memory)
+        words = list(map(lambda each:each.strip('\n'), words0))
+        
+def get_source_word():
+    results = []
     for w in words:
-        w = w.lower().strip()
-        if len(w) == length:
-            got_one = True
-            for loc, let in letters.items():
-                if w[loc-1] != let:
-                    got_one = False
-            if got_one:
-                results.add(w)
-    return sorted(results)
-
-# using: sortedwords = sorted(words, key=len)
-#        print("The longest word in the list is: %s." % (sortedwords[-1],))
-# Ensures at least one valid word (I can see the longest length word in the list is 23 chars (include '))
-size = randrange(6,23)
-source_word = "".join('-' for i in range(size))
-results = suggest(source_word)
-# get length of results and pick an element with random choice from 0 to length - 1
-source_word = results[randrange(0,len(results) - 1)]
+        if (len(w) > 6):
+            results.append(w)
+    source = choice(results)
+    return (source)
 
 
 # In[ ]:
 
 
-from flask import Flask, render_template, request
 app = Flask(__name__)
+app.secret_key = 'OneSessionToRuleThemAll'
 @app.route('/displayform')
 def display_form():
-    return render_template('first.html', source_word=source_word)
+    session['source_word'] = get_source_word()
+    return render_template('first.html', source_word=session['source_word'])
 @app.route('/processform', methods=['POST'])
 def process_form():
     with open('webapp.log', 'a') as logf:
@@ -62,10 +39,4 @@ def process_form():
                             guesses=request.form['guesses'], 
                             the_title='Thanks!')
 app.run(debug=True)
-
-
-# In[ ]:
-
-
-
 
